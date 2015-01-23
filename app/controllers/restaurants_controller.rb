3caa34d7f990a2c1a49c4e7db133d3f4cc81520d
dash-c <deangreyhame@gmail.com>
2015-01-23 16:05:33 +0100

    restyle restaurant show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,7 +12,7 @@ class RestaurantsController < ApplicationController
   def show
     @reviews = Review.where(restaurant_id: @restaurant.id).order("created_at DESC")
     if @reviews.blank?
-      @avg_rating = O
+      @avg_rating = 0
     else
       @avg_rating = @reviews.average(:rating).round(2) 
     end
