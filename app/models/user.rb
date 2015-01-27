a0ebaf27a8debdfc9cbca2d5a873837a1c6d6c63
dash-c <deangreyhame@gmail.com>
2015-01-27 20:09:36 +0100

    corrected registration issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews, dependent: :destroy
+
   validates :firs_name, :last_name, presence: true
-  validates :comment, :rating, presence: true
 end
